4fb886e762670724dcff7ce658c950dfd7de04d5
Dongdong Kong <kongdd@users.noreply.github.com>
2022-06-09 17:56:06 +0800

    improve plot_runoff

--- plot_runoff.m
+++ plot_runoff.m
@@ -15,40 +15,45 @@ function [ax_q, ax_prcp] = plot_runoff(prcp, Qobs, Qsim) % ylim, ylim2
 
     pos = get(0, 'ScreenSize'); % screensize
     figure('color', 'w', 'position', pos);
-    plot(x, Qsim - Qobs, 'g-', 'linewidth', 2); hold on; % Q_bias
-    plot(x, Qsim, 'r-', 'linewidth', 2)
+    
+    % [haxes, hbar, hline] = plotyy(x, prcp, x, Qobs, 'bar', 'plot');
+    % ax_q = haxes(2);
+    % ax_prcp = haxes(1);
 
     [haxes, hline, hbar] = plotyy(x, Qobs, x, prcp, 'plot', 'bar');
     ax_q = haxes(1);
     ax_prcp = haxes(2);
     % set(hline1,'linestyle',':')
-
+    
     %% bar adjust
+    axes(ax_prcp)
+
     set(hbar, 'FaceColor', 'b', 'EdgeColor', 'b')
+    axis ij
+    set(gca, 'fontname', fontname, 'fontsize', 10)
+    set(gca, 'YAxisLocation', 'right')
+    % set(gca, 'ylim', [0, 120])
+    set(gca, 'ycolor', 'b')
+    % set(gca, 'yticklabel', {'0', '5', '10', '15', '20', '25', '30'})
+    ylabel('Precipitation (mm/h)', 'fontname', fontname, 'fontsize', 12, ...
+        'Rotation', 270, 'VerticalAlignment', 'bottom', 'Color', 'b')
+    xlabel('Time', 'fontname', fontname, 'fontsize', 12)
+%     axes(ax_q)
+    % title(title, 'fontname', fontname, 'fontsize', 14)
 
     %% plot adjust
-    axes(ax_q)
+    axes(ax_q); hold on
+    set(gca, 'YAxisLocation', 'left')
+    plot(x, Qsim, 'r-', 'linewidth', 2)
+    plot(x, Qsim - Qobs, 'g-', 'linewidth', 2); hold on; % Q_bias
+
     set(gca, 'fontname', fontname, 'fontsize', 11)
     % set(gca, 'ylim', [-300 900])
     % set(gca, 'ytick', -300:100:500)
     % set(gca, 'yticklabel', {'-300', '-200', '-100', '0', '100', '200', '300', '400', '500'})
     set(gca, 'xticklabel', {''})
     ylabel('Q (m^3/s)')
-    legend('Bias', 'Q_sim', 'Qobs', 'location', 'east')
+    legend('Q_{obs}', 'Q_{sim}', 'Bias', 'location', 'east')
     set(hline, 'linewidth', 1.5, 'color', 'b', 'lineStyle', '--')
     grid on
-    
-    axes(ax_prcp)
-    axis ij
-    set(gca, 'fontname', fontname, 'fontsize', 10)
-    set(gca, 'YAxisLocation', 'left')
-    % set(gca, 'ylim', [0, 120])
-    % set(gca, 'ytick', 0:5:30)
-    % set(gca, 'yticklabel', {'0', '5', '10', '15', '20', '25', '30'})
-    ylabel('Precipitation (mm/h)', 'fontname', fontname, 'fontsize', 12)
-    set(get(gca, 'ylabel'), 'position', [-20 18 10])
-    xlabel('Time', 'fontname', fontname, 'fontsize', 12)
-
-    axes(ax_q)
-    % title(title, 'fontname', fontname, 'fontsize', 14)
 end
